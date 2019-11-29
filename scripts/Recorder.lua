0a427c7b5ae88ae7b17f1c7de002db5c62e245a6
FabianHemer <fabian.hemer@hs-furtwangen.de>
2019-11-29 14:20:55 +0100

    receiver

--- scripts/Recorder.lua
+++ scripts/Recorder.lua
@@ -2,3 +2,8 @@
 local path = "public/"            -- path to save the recorded data in
 local gFilename = "Recording"      -- filenames used for recoded data
 local filetype = "json"        -- msgpack or json
+
+local gRecordingStopped = true --true while recording scans
+local gPlaybackStopped = true
+local gPlaybackPaused = false
+local gEnableLoop = false
