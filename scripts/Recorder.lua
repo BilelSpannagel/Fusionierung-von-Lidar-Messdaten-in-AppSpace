0851b271dc341c0129787d102fa4308b7de3fdcf
FabianHemer <fabian.hemer@hs-furtwangen.de>
2019-11-29 14:18:24 +0100

    receiver

---
+++ scripts/Recorder.lua
@@ -0,0 +1,4 @@
+
+local path = "public/"            -- path to save the recorded data in
+local gFilename = "Recording"      -- filenames used for recoded data
+local filetype = "json"        -- msgpack or json
