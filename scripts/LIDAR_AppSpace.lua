c1b6e6bec6d3f20d1c01192fcaeb5a90b89582a2
Andre Grevinga <andre.grevinga@gmail.com>
2019-11-30 19:53:35 +0100

    Add tests to main

--- scripts/LIDAR_AppSpace.lua
+++ scripts/LIDAR_AppSpace.lua
@@ -2,10 +2,12 @@ local function main()
 
   local PointCloudViewer = require("ViewerModule")
   local Triangle = require("Triangle")
-  local cloud = PointCloud.create()
-  cloud = Triangle.createTriangle()
-  print(cloud)
-  PointCloudViewer.PointCloudViewer(cloud)
+  local RemovePointsBeyond = require("RemovePointsBeyond")
+  local GetMinDistanceAndEdgeLengths = require("GetMinDistanceAndEdgeLengths")
+  local cloud = Triangle.createTriangle()
+  local changedCloud = RemovePointsBeyond.removePointsBeyond(cloud, 750)
+  print(GetMinDistanceAndEdgeLengths.getMinDistanceAndEdgeLengths(changedCloud))
+  PointCloudViewer.PointCloudViewer(changedCloud)
 
 end
 Script.register("Engine.OnStarted", main)
--- scripts/removePointsBeyond.lua
+++ scripts/removePointsBeyond.lua
@@ -3,10 +3,10 @@ local RemovePointsBeyond = {}
 --@removePointsBeyond(inputCloud: PointCloud, maxDistance: double):PointCloud
 function RemovePointsBeyond.removePointsBeyond(inputCloud, maxDistance)
   local resultCloud = PointCloud.create()
-  local size, width, height = inputCloud:getSize()
+  local size, _, _ = inputCloud:getSize()
   for i = 1, (size - 1) do
     local point, intensity = inputCloud:getPoint3D(i)
-    local distance, totalDistance = Point.getDistance(point, Point.create(0, 0, 0))
+    local distance, _ = Point.getDistance(point, Point.create(0, 0, 0))
     if ( distance < maxDistance) then
       resultCloud:appendPoint(point:getX(), point:getY(), point:getZ(), intensity)
     end
