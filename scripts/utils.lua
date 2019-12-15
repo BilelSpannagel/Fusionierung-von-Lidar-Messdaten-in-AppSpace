741247d59d70606e7893ae7c0714da241360c013
FabianHemer <fabian.hemer@hs-furtwangen.de>
2019-12-15 15:48:34 +0100

    Tatsaechliche Messung

--- scripts/utils.lua
+++ scripts/utils.lua
@@ -1,7 +1,8 @@
 local utils = {}
 
-utils.predifinedSideLengths = {100,180,150}
-utils.predifinedAngle = {56.31,33.69, 90}
+utils.predifinedSideLengths = {100,170,140}
+utils.predifinedAngle = {55,35, 90}
+--utils.predifinedAngle = {56.31,33.69, 90}
 utils.originPoint = Point.create(0, 0, 0)
 utils.showMaster = false
 
