264715c8839397b890c6595f11ccf84d1f5eb7b7
Michael Lorens <michael.lorens@hs-furtwangen.de>
2019-12-15 11:22:34 +0100

    feat: add buttons to change to slave/master scan

--- scripts/ViewerModule.lua
+++ scripts/ViewerModule.lua
@@ -1,35 +1,37 @@
 local ViewerModule = {}
 -- luacheck: globals Viewer
-Viewer = View.create("scanViewer")
 
-function ViewerModule.PointCloudViewer(cloud)
-  Viewer:view(cloud)
-end
-
--- luacheck: globals numScans scans pointCloudDecoration transformer
+-- luacheck: globals numScans scans pointCloudDecoration transformer ViewerModule.lastScan
+ViewerModule.lastScan = 0
 numScans = 0
 scans = {}
 pointCloudDecoration = View.PointCloudDecoration.create()
-View.PointCloudDecoration.setPointSize(pointCloudDecoration, 3)
+pointCloudDecoration:setPointSize(3)
+pointCloudDecoration:setXColormap(0)
 transformer = Scan.Transform.create()
-Viewer:setDefaultDecoration(pointCloudDecoration)
+ViewerModule.Viewer = View.create("scanViewer")
+ViewerModule.Viewer:setDefaultDecoration(pointCloudDecoration)
+
+
+function ViewerModule.PointCloudViewer(cloud)
+ViewerModule.Viewer:addPointCloud(cloud)
+ViewerModule.Viewer:present()
+end
 
 --@showScans(scan:Scan):function
 function ViewerModule.showScans(scan)
   --draw scans
-  local function drawScans()
-    Viewer:clear()
-    local clouds = {}
-    clouds[0] = Scan.Transform.transformToPointCloud(transformer, scans[0])
-    clouds[1] = Scan.Transform.transformToPointCloud(transformer, scans[1])
-    clouds[2] = Scan.Transform.transformToPointCloud(transformer, scans[2])
-    clouds[3] = Scan.Transform.transformToPointCloud(transformer, scans[3])
-    View.addPointCloud(Viewer, clouds[0])
-    View.addPointCloud(Viewer, clouds[1])
-    View.addPointCloud(Viewer, clouds[2])
-    View.addPointCloud(Viewer, clouds[3])
+  function drawScans()
+    --Viewer:clear()
+    local cloud = Scan.Transform.transformToPointCloud(transformer, scans[0])
+    ViewerModule.lastScan = cloud
+    cloud = cloud:merge(Scan.Transform.transformToPointCloud(transformer, scans[1]))
+    cloud = cloud:merge(Scan.Transform.transformToPointCloud(transformer, scans[2]))
+    cloud = cloud:merge(Scan.Transform.transformToPointCloud(transformer, scans[3]))
     
-    Viewer:present()
+    ViewerModule.Viewer:addPointCloud(cloud)
+        
+    ViewerModule.Viewer:present()
     scans = {}
     numScans = 0
   end
@@ -40,7 +42,6 @@ function ViewerModule.showScans(scan)
     drawScans();
   end
 end
-
 
 return ViewerModule
 
