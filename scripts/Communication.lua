2408e53d0b81bcb5d94b2f9c3b7aeb50f92984de
Michael Lorens <michael.lorens@hs-furtwangen.de>
2019-12-15 11:21:09 +0100

    feat: add buttons to change the view from master to slave

--- scripts/Communication.lua
+++ scripts/Communication.lua
@@ -1,20 +1,31 @@
 local Communication = {}
 
 socket = UDPSocket.create()
+UDPSocket.bind(socket, 2111)
 
---@receiveScans(_scanner: Scann.Provider.Scanner, masterIP: String):void
-function Communication.receiveScans(_handleOnReceive)
-  -- luacheck: globals receiveHandle
-  receiveHandle = _handleOnReceive
-  --@hndOnReceive(data, ipaddess, port): void
-  function hndOnReceive(data,ipaddress,port)
-    local scan = Object.deserialize(data, "MSGPACK")
-    receiveHandle(scan)
-  end
+--@receiveScans(_handleOnReceive: Handle):void
 
-  UDPSocket.bind(socket, 2111)
-  UDPSocket.register(socket, "OnReceive", "hndOnReceive")
+function handle(data,ipaddress,port)
+  local scan = Object.deserialize(data, "MSGPACK")
+  receiveHandle(scan)
 end
+
+function Communication.receiveScans(_handleOnReceive)
+-- luacheck: globals receiveHandle
+  receiveHandle = _handleOnReceive
+
+  --@hndOnReceive(data, ipaddess, port): void
+
+
+  UDPSocket.register(socket, "OnReceive", "handle")
+  print "Socket bound"
+end
+
+--@functionname(p1:type):returnType
+function Communication.stopReceiving()
+  socket:deregister("OnReceive", "handle")
+end
+
 
 --@sendScans(_scanner: Scann.Provider.Scanner, masterIP: String):void
 function Communication.sendScans(_scanProvider, _masterIP)
@@ -24,11 +35,8 @@ function Communication.sendScans(_scanProvider, _masterIP)
     local scanObj = Object.serialize(scan, "MSGPACK")
     UDPSocket.transmit(socket, _masterIP, 2111, scanObj)
   end
-  -- "192.168.1.10"
-
   _scanProvider:register("OnNewScan", handleOnNewScan)
   --_scanProvider:start()
-  print "Scanner started"
 end
 
 return Communication
